0c0cfeabf3d0623630bfafea151b82db51e29a21
Roman Stadler <rosta99x@gmx.ch>
2020-09-16 15:40:03 +0200

    updated Main for testing syntax

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         //testclass
         System.out.println("Hello World");
+        for(int i=0; i<10;i++){
+            System.out.println(i);
+
+        }
     }
 }

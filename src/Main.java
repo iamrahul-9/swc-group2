0f213a184d2b8991987c1e044cf12918a75a88c2
Strawberry17 <45404454+Strawberry17@users.noreply.github.com>
2020-09-16 15:48:56 +0200

    said hi

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         //testclass
         System.out.println("Hello World");
         for(int i=0; i<10;i++){
-            System.out.println("Hi!");
+            System.out.println(i);
 
         }
+        System.out.println("Hi!");
     }
 }
